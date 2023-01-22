e1cf52b6365f3e6c0351adc839ed654828663e74
AbdussamadYisau <sammieyisau@gmail.com>
2023-01-22 01:20:17 +0100

    Started work on navbar, remains divider between font picker and theme switcher

--- dictionary-app/.next/static/chunks/webpack.js
+++ dictionary-app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "5b0b1a915531ce1a"; }
+/******/ 		__webpack_require__.h = function() { return "0acc865c3ee934f9"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- dictionary-app/pages/index.js
+++ dictionary-app/pages/index.js
@@ -1,42 +1,107 @@
-import Head from 'next/head'
+import Head from "next/head";
 import { useTheme } from "next-themes";
-import styles from  "../styles/Home.module.css";
-import { useState , useEffect} from 'react';
+import styles from "../styles/Home.module.css";
+import { useState, useEffect } from "react";
+import Logo from "../components/Logo";
+import Moon from "../components/Moon";
+import { Inter, Lora, Inconsolata } from "@next/font/google";
 
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+
+const lora = Lora({
+  subsets: ["latin"],
+  variable: "--font-lora",
+});
+
+const inconsolata = Inconsolata({
+  subsets: ["latin"],
+  variable: "--font-inconsolata",
+});
+
+const FontPicker = () => {
+  const [selectedFont, setSelectedFont] = useState(inter.variable);
+
+  const handleFontChange = (e) => {
+    setSelectedFont(e.target.value);
+  };
+
+  return (
+    <div className="select">
+      <select
+        value={selectedFont}
+        onChange={handleFontChange}
+        className="font-bold text-lg"
+      >
+        <option value={inter.variable}>Inter</option>
+        <option value={lora.variable}>Lora</option>
+        <option value={inconsolata.variable}>Inconsolata</option>
+      </select>
+      <style jsx global>{`
+        body {
+          font-family: ${selectedFont};
+        }
+      `}</style>
+    </div>
+  );
+};
 
 export default function Home() {
-  const [mounted, setMounted] = useState(false)
-  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
 
-    // useEffect only runs on the client, so now we can safely show the UI
-    useEffect(() => {
-      setMounted(true)
-    }, [])
-  
-    if (!mounted) {
-      return null
-    }
-  
-  
+  const { theme, setTheme } = useTheme();
+
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
   return (
     <>
-    <Head>
-      <meta charSet='UTF-8'/>
-      <meta httpEquiv='X-UA-Compatible' content='IE=edge'/>
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 
-      <title>Dictionary App</title>
-      
-    </Head>
+        <title>Dictionary App</title>
+      </Head>
 
-    <div className='main-container flex justify-between mx-[351px]'>
-      <p className='text-3xl text-black dark:text-white'>Hello</p>
-      <select value={theme} onChange={e => setTheme(e.target.value)}>
-        <option value="dark">Dark</option>
-        <option value="light">Light</option>
-      </select>
-    </div>
+      <div
+        className={` ${styles.navbar} flex justify-between items-center mx-[351px]`}
+      >
+        <Logo stroke={"#757575"} fill={"#757575"} />
 
+        <div className="flex justify-between gap-[12px] items-center">
+          <FontPicker />
+
+          <div className="flex gap-[20px] items-center">
+            <input
+              type="checkbox"
+              name="checkbox"
+              id="checkbox"
+              class="toggle__checkbox"
+            />
+            <label
+              for="checkbox"
+              class="toggle"
+              style={{
+                backgroundColor: theme === "light" ? "#757575" : "#A445ED",
+              }}
+              onClick={() => {
+                theme === "light" ? setTheme("dark") : setTheme("light");
+              }}
+            ></label>
+
+            <Moon stroke={theme === "light" ? "#757575" : "#A445ED"} />
+          </div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
