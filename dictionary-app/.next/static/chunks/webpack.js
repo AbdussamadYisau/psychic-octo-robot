8170ae91c4eceb133f9af3c04081dd783ff727e4
AbdussamadYisau <sammieyisau@gmail.com>
2023-01-23 02:04:34 +0100

    Might have made some progress on api front?

--- dictionary-app/.next/static/chunks/webpack.js
+++ dictionary-app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "2b94e18de0833c8b"; }
+/******/ 		__webpack_require__.h = function() { return "233b04b44d962ba0"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
