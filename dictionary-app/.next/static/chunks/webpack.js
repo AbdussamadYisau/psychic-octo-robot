81dc1e7963adf797baf363df3a378187b6043e51
AbdussamadYisau <sammieyisau@gmail.com>
2023-01-26 16:50:30 +0100

    Think i'm done with mobile responsiveness, we move to tablet

--- dictionary-app/.next/static/chunks/webpack.js
+++ dictionary-app/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a7cc36e922b0350d"; }
+/******/ 		__webpack_require__.h = function() { return "c5b4e085067dd3eb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
